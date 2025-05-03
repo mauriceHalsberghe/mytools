9a63f82e2f080da7a8fda0e8601324da16b7835d
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-05-04 00:31:21 +0200

    added basic css styling

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,15 @@
 const userId = 1;
-const URL = "https://mytools-server.onrender.com/"
+const URL = "https://mytools-server.onrender.com"
 
-async function getUsers() {
+async function getData(slug) {
     try {
-      const response = await fetch(`${URL}/users`, {
+      const response = await fetch(`${URL}/${slug}`, {
         method: "GET",
       });
 
       if (response.status === 200) {
         const jsonData = await response.json();
-        console.log(jsonData);
+        return jsonData;
         
       } else {
         throw new Error("Something went wrong");
@@ -19,4 +19,29 @@ async function getUsers() {
     }
 }
 
-getUsers()
+async function showImages() {
+  const $imageHtml = document.querySelector('.images');
+  const data = await getData('images');
+
+  data.forEach(image => {    
+    const dateObj = new Date(image.image_date);
+    const formattedDate = dateObj.toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    });
+
+    $imageHtml.innerHTML += `
+      <figure>
+        <img src='${image.url}' alt='${image.title}'>
+        <figcaption>
+          <h2>${image.title}</h2>
+          <p>${formattedDate}</p>
+        </figcaption>
+      </figure>
+    `;
+  });
+}
+
+
+showImages()
