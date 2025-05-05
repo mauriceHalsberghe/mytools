36cf9397b6a7fe67aa40fe76ab449a79a9788448
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-05-05 12:02:54 +0200

    added favicon

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,9 +1,16 @@
 const $menuButton = document.querySelector('.button--menu');
 const $nav = document.querySelector('.nav');
+
+const $burger = document.querySelector('.button__burger');
+const $close = document.querySelector('.button__close');
+
 $menuButton.addEventListener('click', () => {
     $nav.classList.toggle('nav--hidden')
+    $close.classList.toggle('button--hidden');
+    $burger.classList.toggle('button--hidden');
 })
 
-window.addEventListener("load", (event) => {
+window.addEventListener("load", () => {
     $nav.classList.add('nav--hidden');
+    $close.classList.add('button--hidden');
 });
